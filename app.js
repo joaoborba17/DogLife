f4d6ebe49a37a12f7e610292ebd9c50b3a4f8de3
joaoborba17 <joaoborba2000@gmail.com>
2017-06-01 15:45:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+import { Constants, Google, Facebook } from 'expo';
+
+export default class App extends Component {
+  _handleButtonPress = () => {
+    Alert.alert(
+      'Button pressed!',
+      'You did it!',
+    );
+  };
+
+  _handleGoogleLogin = async () => {
+    try {
+      const { type, user } = await Google.logInAsync({
+        androidStandaloneAppClientId: '<ANDROID_CLIENT_ID>',
+        iosStandaloneAppClientId: '<IOS_CLIENT_ID>',
+        androidClientId: '603386649315-9rbv8vmv2vvftetfbvlrbufcps1fajqf.apps.googleusercontent.com',
+        iosClientId: '603386649315-vp4revvrcgrcjme51ebuhbkbspl048l9.apps.googleusercontent.com',
+        scopes: ['profile', 'email']
+      });
+
+      switch (type) {
+        case 'success': {
+          Alert.alert(
+            'Logged in!',
+            `Hi ${user.name}!`,
+          );
+          break;
+        }
+        case 'cancel': {
+          Alert.alert(
+            'Cancelled!',
+            'Login was cancelled!',
+          );
+          break;
+        }
+        default: {
+          Alert.alert(
+            'Oops!',
+            'Login failed!',
+          );
+        }
+      }
+    } catch (e) {
+      Alert.alert(
+        'Oops!',
+        'Login failed!',
+      );
+    }
+  };
+
+  _handleFacebookLogin = async () => {
+    try {
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+        '1201211719949057', // Replace with your own app id in standalone app
+        { permissions: ['public_profile'] }
+      );
+
+      switch (type) {
+        case 'success': {
+          // Get the user's name using Facebook's Graph API
+          const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+          const profile = await response.json();
+          Alert.alert(
+            'Logged in!',
+            `Hi ${profile.name}!`,
+          );
+          break;
+        }
+        case 'cancel': {
+          Alert.alert(
+            'Cancelled!',
+            'Login was cancelled!',
+          );
+          break;
+        }
+        default: {
+          Alert.alert(
+            'Oops!',
+            'Login failed!',
+          );
+        }
+      }
+    } catch (e) {
+      Alert.alert(
+        'Oops!',
+        'Login failed!',
+      );
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.destaque}>
+        DogLife
+        </Text>
+          
+      <View style={styles.button}>
+          <Button
+            title="Entrar com o Google"
+            onPress={this._handleGoogleLogin}
+          />
+      </View>
+      <View style={styles.button}>
+       <Button
+          title="Entrar com o Facebook"
+          onPress={this._handleFacebookLogin}
+        />
+      </View>
+      
+      
+      
+      
+      
+      
+      
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  destaque: {
+    color: "#003311", 
+    fontSize: 40
+    
+    
+  }, 
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+    },
+button:{
+  marginTop: 20
+}
+  
+  
+});
+
